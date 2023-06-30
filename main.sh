c0f4a3c8223c8434af466ed5721a64ec2addb1d6
一只鬆 <yzsong06@outlook.com>
2023-06-30 20:14:00 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,5 @@
 export MYSQL_HOME=$HOME/$REPL_SLUG/data
 mysqld_safe &
+# 防休眠命令
+while true; do curl -s --user-agent "${UA_Browser}" "https://${REPL_SLUG}.${REPL_OWNER}.repl.co" >/dev/null 2>&1 && echo "$(date +'%Y%m%d%H%M%S') Keeping online ..." && sleep 600; done &
 php -S 0.0.0.0:443 -t .
