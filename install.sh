68e406c8a6300294f9939b18d59d4b7fbbc3fc09
一只鬆 <yzsong06@outlook.com>
2023-06-30 19:59:30 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
 git clone https://github.com/yzsong06/Replit-WordPress_Zh_CN/ && mv -b Replit-WordPress_Zh_CN/* ./ && mv -b Replit-WordPress_Zh_CN/.[^.]* ./ && rm -rf *~ && rm -rf Replit-WordPress_Zh_CN
+sh build.sh
